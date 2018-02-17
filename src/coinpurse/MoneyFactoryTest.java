21d0879a76d9e5a8ecf722046f16ac10275b7941
Theeruth Borisuth <pwnimatorz@hotmail.com>
2018-02-17 19:37:06 +0700

    change MoneyFactoryTest that looks like PurseTest

--- src/coinpurse/MoneyFactoryTest.java
+++ src/coinpurse/MoneyFactoryTest.java
@@ -1,6 +1,8 @@
-
 package coinpurse;
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
 import org.junit.Test;
 
 /**
@@ -16,48 +18,194 @@ import org.junit.Test;
  */
 public class MoneyFactoryTest {
 	/** tolerance for comparing two double values */
-
 	private static final double TOL = 1.0E-6;
-	private static final String CURRENCY = "Baht";
+	private MoneyFactory mf;
 	
     /**
      * Sets up the test fixture.
      * Called before every test method.
      */
-    
-    public void setUp() {
-    	// nothing to initialize
+    public void testSetandgetInstance() {
+    	MoneyFactory.setMoney(new ThaiMoneyFactory());
+    	mf = MoneyFactory.getInstance();
     }
     
     
+
+    /** Insert some coins. Easy test. */
     @Test
-    public void testSetAndgetInstance(){
-    	MoneyFactory.setMoney(new ThaiMoneyFactory());
-    	assertEquals(MoneyFactory.getInstance().getClass().getSimpleName(),"ThaiMoneyFactory");
-   
-    }
-    
-    @Test
-    public void testCreateMoneySerial(){
-    	MoneyFactory.setMoney(new ThaiMoneyFactory());
-    	MoneyFactory mf = MoneyFactory.getInstance();
-    	Valuable bk1 = mf.createMoney(100);
-    	Valuable bk2 = mf.createMoney("500");
-    	assertEquals(((BankNote)bk1).getSerial(), 1000000);
-    	assertEquals(((BankNote)bk2).getSerial(), 1000001);
-    }
-    
-    
-    @Test(expected = IllegalArgumentException.class)
-    public void testExceptionCreateMoney(){
-    	MoneyFactory.setMoney(new ThaiMoneyFactory());
-    	MoneyFactory mf = MoneyFactory.getInstance();
-    	Valuable bk3 = mf.createMoney("hello");
-    	assertEquals(((BankNote)bk3).getSerial(), 1000000);
+    public void testInsert()
+    {
+        Purse purse = new Purse(3);
+
+        assertTrue( purse.insert(mf.createMoney(5)));
+        assertTrue( purse.insert(mf.createMoney("10")));
+        assertTrue( purse.insert(mf.createMoney(20)));
+        assertEquals( 3, purse.count() );
+        // purse is full so insert should fail
+        assertFalse( purse.insert(mf.createMoney(1000)) );
 
     }
-    
-    
+
+
+
+    @Test(timeout=1000)
+    public void testIsFull()
+    {   // borderline case (capacity 1)
+        Purse purse = new Purse(1);
+        assertFalse( purse.isFull() );
+        purse.insert( mf.createMoney(1) );
+        assertTrue( purse.isFull() );
+        // real test
+        int capacity = 4;
+        purse = new Purse(capacity);
+        for(int k=1; k<=capacity; k++) {
+            assertFalse(purse.isFull());
+            purse.insert( mf.createMoney(k) );
+        }
+        // should be full now
+        assertTrue( purse.isFull() );
+        assertFalse( purse.insert( mf.createMoney(5) ) );
+    }
+
+	/** Should be able to insert same coin many times,
+	 *  since spec doesn't say anything about this.
+	 */
+	@Test(timeout=1000)
+	public void testInsertSameCoin()
+	{
+		int capacity = 5;
+		double value = 10.0;
+		Purse purse = new Purse(capacity);
+		assertTrue( purse.insert(mf.createMoney(value)) );
+		assertTrue( purse.insert(mf.createMoney(value)) ); // should be allowed
+		assertTrue( purse.insert(mf.createMoney(value)) ); // should be allowed
+		assertTrue( purse.insert(mf.createMoney(value)) ); // should be allowed
+		assertTrue( purse.insert(mf.createMoney(value)) ); // should be allowed
+		assertEquals( purse.getBalance(), 5*value, TOL);
+	}
+
+	/** Add one coin and remove it. */
+	@Test(timeout=1000)
+	public void testEasyWithdraw() {
+		Purse purse = new Purse(10);
+		double [] values = {1, 20, 0.5, 10}; // values of coins we will insert
+		
+		for(double value : values) {
+			Valuable coin = mf.createMoney(value);
+			assertTrue(purse.insert(coin));
+			assertEquals(value,  purse.getBalance(), TOL);
+			Valuable [] result = purse.withdraw(value);
+			assertTrue( result != null );
+			assertEquals( 1, result.length );
+			assertSame(  coin, result[0] ); // should be same object
+			assertEquals( 0, purse.getBalance(), TOL );
+		}
+	}
+	
+
+	/** Add 4 coins and then withdraw in pairs, but not in same order. */
+	@Test(timeout=1000)
+	public void testMultiWithdraw() {
+		Purse purse = new Purse(10);
+		Valuable[] coins = { mf.createMoney(5.0), mf.createMoney(10.0), mf.createMoney(1.0), mf.createMoney(5.0) };
+		// insert them all
+		for(Valuable coin: coins) assertTrue( purse.insert(coin) );
+		
+		double amount1 = coins[1].getValue() + coins[3].getValue();
+		double amount2 = coins[0].getValue() + coins[2].getValue();
+		assertEquals(amount1+amount2, purse.getBalance(), TOL );
+		
+		Valuable [] wd1 = purse.withdraw(amount1);
+		assertEquals(amount1, sum(wd1), TOL );
+		
+		assertEquals(amount2, purse.getBalance(), TOL );
+		Valuable [] wd2 = purse.withdraw(amount2);
+		
+		// should be empty now
+		assertEquals(0, purse.getBalance(), TOL );
+	}
+	
+
+	/** Withdraw full amount in purse, using varying numbers of objects. */
+	@Test(timeout=1000)
+	public void testWithdrawEverything() {
+		Purse purse = new Purse(10);
+		// Coins we want to insert and then withdraw.
+		// Use values such that greedy will succeed, but not monotonic
+		List<Valuable> coins = Arrays.asList(
+				mf.createMoney(1.0), mf.createMoney(0.5), mf.createMoney(10.0), mf.createMoney(0.25), mf.createMoney(5.0)
+				);
+		// num = number of coins to insert and then withdraw
+		for(int num=1; num <= coins.size(); num++) {
+			double amount = 0.0;
+			List<Valuable> subList = coins.subList(0, num);
+			for(Valuable c: subList) {
+				purse.insert(c);
+				amount += c.getValue();
+			}
+			// balance should be exactly what we just inserted
+			assertEquals( amount, purse.getBalance(), TOL);
+			// can we withdraw it all?
+			Valuable[] result = purse.withdraw(amount);
+			String errmsg = String.format("couldn't withdraw %.2f but purse has %s",
+					amount, Arrays.toString(subList.toArray()) );
+			assertNotNull( errmsg, result );
+			// is the amount correct?
+			assertEquals("Withdraw wrong amount", amount, sum(result), TOL);
+			// should not be anything left in the purse
+			assertEquals( 0.0, purse.getBalance(), TOL);
+		}
+	}
+	
+	@Test(timeout=1000)
+	public void testSatang(){
+		Purse p = new Purse(3);
+		p.insert(mf.createMoney(0.25));
+		p.insert(mf.createMoney("0.5"));
+		for (Valuable money : p.getMoney()){
+			assertEquals("Baht", money.getCurrency());
+		}
+	}
+	
+	@Test(timeout=1000)
+	public void testSington(){
+		MoneyFactory mf1 = MoneyFactory.getInstance();
+		MoneyFactory mf2 = MoneyFactory.getInstance();
+		assertTrue(mf1 == mf2);
+	}
+	
+	@Test(timeout=1000)
+	public void testSerial(){
+		Valuable money1 = mf.createMoney(20);
+		Valuable money2 = mf.createMoney("20");
+		
+		assertNotEquals(money1.toString(), money2.toString());
+	}
+	
+	@Test(timeout=1000)
+	public void testImpossibleWithdraw() {
+		Purse purse = new Purse(10);
+		assertNull( purse.withdraw(1) );
+		purse.insert( mf.createMoney(20) );
+		assertNull( purse.withdraw(1) );
+		assertNull( purse.withdraw(19) );
+		assertNull( purse.withdraw(21) );
+		purse.insert( mf.createMoney(20) ); // now it has 20 + 20
+		assertNull( purse.withdraw(30) );
+	}
+	
+	/**
+	 * Sum the value of some coins.
+	 * @param wd1 array of coins
+	 * @return sum of values of the coins
+	 */
+	private double sum(Valuable[] wd1)  {
+		if (wd1 == null) return 0.0;
+		double sum = 0;
+		for(Valuable c: wd1) if (c != null) sum += c.getValue();
+		return sum;
+	}
 }
 
 
