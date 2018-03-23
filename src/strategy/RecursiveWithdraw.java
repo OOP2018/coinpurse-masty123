2524ca53715102d2ff01adc80e41a0d232c719af
Theeruth Borisuth <pwnimatorz@hotmail.com>
2018-03-23 15:12:49 +0700

    edit more recursive class

--- src/strategy/RecursiveWithdraw.java
+++ src/strategy/RecursiveWithdraw.java
@@ -13,29 +13,28 @@ public class RecursiveWithdraw implements WithdrawStrategy{
 
 	@Override
 	public List<Valuable> withdraw(Valuable amount, List<Valuable> money) {
-		if(amount.getValue() == 0) return new ArrayList<Valuable>();
-		if(money.isEmpty()) return null;
-		if(money != null){
-		
-		//First Case
-		Valuable remaining = new Money(amount.getValue() - money.get(0).getValue(), amount.getCurrency());
-		List<Valuable> wd1 = withdraw(remaining , money.subList(1, money.size()));
-		if(wd1 != null){
+		if(money.isEmpty()) return null;			
+		if(money.size() > 0){
+			//First Case
+			Valuable remaining = new Money(amount.getValue() - money.get(0).getValue(), amount.getCurrency());
+			List<Valuable> wd1 = withdraw(remaining , money.subList(1, money.size()));
 			List<Valuable> temp = new ArrayList<Valuable>();
-			temp.add(money.get(0));
-			temp.addAll(wd1);
-			return temp;
+
+				if(wd1 != null){
+					temp.add(money.get(0));
+					temp.addAll(wd1);
+					return temp;
+			   }
+			
+			//Second Case
+			List<Valuable> wd2 = withdraw(amount , money.subList(1, money.size()));
+				if(wd2 != null){
+					temp.add(money.get(0));
+					temp.addAll(wd2);
+					return temp;
+			   }
+		   }	
+		
+		  return null;
 		}
-		
-		//Second Case
-		List<Valuable> wd2 = withdraw(amount , money.subList(1, money.size()));
-			if(wd2 != null){
-			List<Valuable> temp = new ArrayList<Valuable>();
-			temp.add(money.get(0));
-			temp.addAll(wd2);
-			return temp;
-			}
-		}	
-		return null;
-	}
 }
